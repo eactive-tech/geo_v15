06b9e4d9a8e3cfb5f7b3a96a97b148fc7f2db8eb
Ayush <ayush@eactive.in>
2024-12-07 16:43:57 +0530

    validate batch stock

--- geo_v15/geo_v15/utils/sales_invoice.py
+++ geo_v15/geo_v15/utils/sales_invoice.py
@@ -5,26 +5,22 @@ def validate_batch_stock(doc, method):
     if doc.update_stock:
         if doc.items:
             for item in doc.items:
-                x = frappe.call(
-                    "frappe.desk.query_report.run",
-                    report_name="Batch-Wise Balance History",
-                    filters={
-                        "company": doc.company,
-                        "from_date": today(),
-                        "to_date": today(),
-                        "item_code":item.item_code,
-                        "warehouose": item.warehouse,
-                        "batch_no": item.batch_no
-                        },
-                    ignore_prepared_report=True
-                    )
-                x["result"].pop()
-                
-                # if x and x.get("result"):
-                #     balance_qty = x["result"][0].get("balance_qty")
-                
-                #     if balance_qty is not None and item.qty > balance_qty:
-                #         frappe.throw(f'Insufficient stock for batch {item.batch_no}. Available: {balance_qty}, Required: {item.qty}')
-                if len(x["result"]) > 0:
-                    if item.qty > x["result"][0].get("balance_qty"):
-                        frappe.throw(f'Stock Insufficient{x["result"][0].get("balance_qty")}')
+                if item.batch_no:
+                    x = frappe.call(
+                        "frappe.desk.query_report.run",
+                        report_name="Batch-Wise Balance History",
+                        filters={
+                            "company": doc.company,
+                            "from_date": today(),
+                            "to_date": today(),
+                            "item_code":item.item_code,
+                            "warehouse": item.warehouse,
+                            "batch_no": item.batch_no
+                            },
+                        ignore_prepared_report=True
+                        )
+                    if x.get("result"):
+                        x["result"].pop()                    
+
+                        if item.qty > x["result"][0].get("balance_qty"):
+                            frappe.throw(f'Stock Insufficient{x["result"][0].get("balance_qty")}')
